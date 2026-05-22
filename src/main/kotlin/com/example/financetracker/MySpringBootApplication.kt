b8719dd3f0e03caa4adfa48ea80d6fb9c942e0fc
VVV <vikisVolkova@gmail.com>
2026-05-22 13:59:04 +0300

    initially created

---
+++ src/main/kotlin/com/example/financetracker/MySpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.example.financetracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MySpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<MySpringBootApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/financetracker/MySpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.financetracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
