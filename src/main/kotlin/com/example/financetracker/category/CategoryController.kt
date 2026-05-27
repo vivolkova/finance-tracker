48919382c3d7d1d6e79dd2e4cb5ba4a959dfe7a8
VVV <vikisVolkova@gmail.com>
2026-05-27 15:57:34 +0300

    docs: swagger supported

--- src/main/kotlin/com/example/financetracker/category/CategoryController.kt
+++ src/main/kotlin/com/example/financetracker/category/CategoryController.kt
@@ -1,25 +1,32 @@
 package com.example.financetracker.category
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/categories")
+@Tag(name = "Categories", description = "Category control")
 class CategoryController(
     private val categoryService: CategoryService
 ) {
 
     @GetMapping
-    fun getAll(): List<Category> =
-        categoryService.getAll()
+    @Operation(summary = "Get all transactions")
+    fun getAll(): List<CategoryDto> = categoryService.getAll()
 
     @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long): Category =
-        categoryService.getById(id)
+    @Operation(summary = "Get transaction by Id")
+    @ApiResponse(responseCode = "200", description = "Category found")
+    @ApiResponse(responseCode = "404", description = "Category not found")
+
+    fun getById(@PathVariable id: Long): CategoryDto = categoryService.getById(id)
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody request: CreateCategoryRequest): Category =
+    fun create(@RequestBody request: CreateCategoryRequest): CategoryDto =
         categoryService.create(request.name, request.type)
 
     @DeleteMapping("/{id}")
