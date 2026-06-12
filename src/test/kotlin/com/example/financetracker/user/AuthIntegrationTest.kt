eb4afbaf7b04e7566d4db9bfb47b31119a66bb2a
VVV <vikisVolkova@gmail.com>
2026-06-12 19:34:44 +0300

    test(auth): add integration tests

--- src/test/kotlin/com/example/financetracker/user/AuthIntegrationTest.kt
+++ src/test/kotlin/com/example/financetracker/user/AuthIntegrationTest.kt
@@ -4,12 +4,13 @@ import com.example.financetracker.IntegrationTestBase
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.assertNotNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.resttestclient.TestRestTemplate
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
 import kotlin.test.Test
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotNull
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthIntegrationTest : IntegrationTestBase() {
@@ -19,7 +20,6 @@ class AuthIntegrationTest : IntegrationTestBase() {
 
     val email = "test@mail.ru"
     val password = "1234"
-
 
     @BeforeAll
     fun `register test`() {
@@ -31,6 +31,22 @@ class AuthIntegrationTest : IntegrationTestBase() {
     }
 
     @Test
+    fun `register test with duplicate email`() {
+        val user = RegisterRequest(email, password)
+        val registerResult = restTemplate.postForEntity("/api/auth/register", user, ProblemDetail::class.java)
+        assertEquals(HttpStatus.BAD_REQUEST, registerResult.statusCode, "Wrong Status Code")
+        assertEquals("User with email $email already exists", registerResult.body?.detail, "Wrong result message")
+    }
+
+    @Test
+    fun `login test with wrong password`() {
+        val user = LoginRequest(email, "password")
+        val loginResult = restTemplate.postForEntity("/api/auth/login", user, ProblemDetail::class.java)
+        assertEquals(HttpStatus.BAD_REQUEST, loginResult.statusCode, "Wrong Status Code")
+        assertEquals("Invalid email or password", loginResult.body?.detail, "Wrong result message")
+    }
+
+    @Test
     fun `login test`() {
         val loginRequest = LoginRequest(email, password)
         val loginResult = restTemplate.postForEntity("/api/auth/login", loginRequest, AuthResponse::class.java)
@@ -39,11 +55,40 @@ class AuthIntegrationTest : IntegrationTestBase() {
     }
 
     @Test
-    fun `wrong email, login test`() {
+    fun `login test with wrong email`() {
         val loginRequest = LoginRequest("email@email", password)
         val loginResult =
             restTemplate.postForEntity("/api/auth/login", loginRequest, ProblemDetail::class.java)
         assertEquals(HttpStatus.BAD_REQUEST, loginResult.statusCode, "Wrong Status Code")
-        assertEquals("Invalid email or password", loginResult.body?.detail)
+        assertEquals("Invalid email or password", loginResult.body?.detail, "Wrong result message")
+    }
+
+    @Test
+    fun `refresh token`() {
+        val loginResult =
+            restTemplate.postForEntity("/api/auth/login", LoginRequest(email, password), AuthResponse::class.java)
+        assertNotNull(loginResult.body?.refreshToken, "Refresh token is empty")
+        assertNotNull(loginResult.body?.accessToken, "Access token is empty")
+
+        val refreshResult = restTemplate.postForEntity(
+            "/api/auth/refresh",
+            RefreshRequest(loginResult.body!!.refreshToken),
+            AuthResponse::class.java
+        )
+        assertEquals(HttpStatus.OK, refreshResult.statusCode)
+        assertNotNull(refreshResult.body?.refreshToken, "No refresh token")
+        assertNotNull(refreshResult.body?.accessToken, "No access token")
+        assertNotEquals(loginResult.body!!.refreshToken, refreshResult.body!!.refreshToken)
+    }
+
+    @Test
+    fun `invalid refresh token`(){
+        val refreshResult = restTemplate.postForEntity(
+            "/api/auth/refresh",
+            RefreshRequest("invalid refresh token"),
+            ProblemDetail::class.java
+        )
+        assertEquals(HttpStatus.BAD_REQUEST, refreshResult.statusCode)
+        assertEquals("Invalid refresh token", refreshResult.body?.detail)
     }
 }
